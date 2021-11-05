cc6214386a9a956525c51f39e974d547d1d86ca7
admin <andrey.perchekliy@gmail.com>
2021-11-05 23:25:38 +0300

    Initial commit

---
+++ src/KalkTest.java
@@ -0,0 +1,25 @@
+public class KalkTest {
+    public class KalkNum {
+        byte test;
+        int num;
+        double ans ;
+        char op;
+        String ravenstvo;
+
+        public KalkNum() {
+            this.test = test;
+            this.num = num;
+            this.ans = ans;
+            this.op = op;
+            this.ravenstvo = ravenstvo;
+        }
+
+        public void setColor(String color) {
+            this.op = op;
+        }
+
+        public String getVinNumber() {
+            return ravenstvo;
+        }
+}
+}
